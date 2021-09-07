befe9a642531eef79262f6728ab7e1be64827c1c
saharshg29 <saharshg29@gmail.com>
2021-09-07 17:40:52 +0530

    MONGO

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -3,5 +3,41 @@
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/playground')
-.then(() => console.log('Connection successfull '))
-.catch(err => console.error('Could not connect ot MongoDB..', err))
+    .then(() => console.log('Connection successfull '))
+    .catch(err => console.error('Could not connect ot MongoDB..', err));
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: { type: Date, default: Date.now },
+    isPublished: Boolean
+});
+
+const Course = mongoose.model('Course', courseSchema);
+// console.log(Course);
+
+async function createCourse() {
+    const course = new Course({
+        name: 'Anggulaar Course',
+        author: 'Saharsh',
+        tags: ['Anggulaar', 'backend'],
+        isPublished: true
+    });
+
+    // console.log(course);
+
+    const result = await course.save();
+    console.log(result)
+}
+
+// createCourse();
+
+// Retrieving data from MongoDB
+
+async function getCourse() {
+    const courses = await Course.find();
+    console.log(courses);
+}
+
+getCourse()
