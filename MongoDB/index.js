ea7018fade065864f591031b35fd583934450c6c
saharshg29 <saharshg29@gmail.com>
2021-09-07 23:45:34 +0530

    mongoose

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -15,7 +15,9 @@ const courseSchema = new mongoose.Schema({
 });
 
 const Course = mongoose.model('Course', courseSchema);
-// console.log(Course);
+
+// Creating a database in MonogoDB
+/*
 
 async function createCourse() {
     const course = new Course({
@@ -31,9 +33,12 @@ async function createCourse() {
     console.log(result)
 }
 
-// createCourse();
+ createCourse();
+
+*/
 
 // Retrieving data from MongoDB
+/*
 
 async function getCourse() {
     const courses = await Course.find();
@@ -41,3 +46,100 @@ async function getCourse() {
 }
 
 getCourse()
+    
+*/
+
+/*
+
+Using find limit sort and select with our files
+
+async function getCourses() {
+    const courses = await Course
+        .find({ isPublished: true })
+        .limit(1)
+        .sort({ name: -1})
+        .select({name: 1, tags: 1 });
+    console.log(courses)
+}
+
+getCourses();
+
+*/
+/*
+async function getCourses() {
+
+    // Comparison operators for mongoDB or Mongoose
+
+    // eq => equal
+    // ne => not equal
+    // gt => greater than 
+    // gte => greater than or equal to
+    // lt => less than 
+    // lte => less than or equal to
+    // in => in
+    // nin => not in
+
+    const courses = await Course
+        // .find({ author: 'Saharsh', isPublished: true})
+        // .find({ price: {$gt : 10, $lte: 20} })
+        .find({ price: { $in: [15, 20, 10, 30] } })
+        .limit(1)
+        .sort({ name: -1 })
+        .select({ name: 1, tags: 1 });
+    console.log(courses)
+}
+
+
+getCourses();
+*/
+
+/*
+async function getCourses() {
+
+    // Logical Operators in MongoDB
+
+    // or
+    // and
+
+    const courses = await Course
+        // .find({ isPublished: true })
+        .find()
+        .or([ {author: 'Mosh'}, {isPublished: true} ])
+        .and([ {author: 'Mosh'}, {isPublished: true} ])
+        .limit(1)
+        .sort({ name: -1})
+        .select({name: 1, tags: 1 });
+    console.log(courses)
+}
+
+getCourses();
+*/
+
+
+async function getCourses() {
+
+    // Regular Expression in MongoDB
+    //are written within (/pattern/)
+    // in order to make the pattern case insensitive use i as expressed here /pattern/i
+
+    const courses = await Course
+        // .find({ isPublished: true })
+
+        //Starts with saharsh
+        .find({ author: /^Saharsh/ })
+
+        //Ends with Gupta
+        .find({ author: /Gupta$/ })
+
+        // Contains harsh
+        .find({ author: /.*harsh*./ })
+
+        .limit(1)
+        .skip()
+        .sort({ name: -1 })
+        .select({ name: 1, tags: 1 })
+        .count();
+    console.log(courses)
+}
+
+getCourses();
